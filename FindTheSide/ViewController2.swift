319730a9253bf3e2373926bd48e9b42052f0a84b
Jianing Fu <jianingfu@gatech.edu>
2018-10-20 03:48:14 -0400

    adf

---
+++ FindTheSide/ViewController2.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController2.swift
+//  FindTheSide
+//
+//  Created by Jianing Fu on 10/20/18.
+//  Copyright © 2018 nosnaCo. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController2: UIViewController, ARSCNViewDelegate {
+
+}
