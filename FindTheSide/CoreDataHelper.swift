b177066b82d71693d317dfe6fbbb4938854e7021
Yujia Gao <yujia911210575@163.com>
2018-10-20 01:32:45 -0400

    add core data

---
+++ FindTheSide/CoreDataHelper.swift
@@ -0,0 +1,24 @@
+//
+//  CoreDataHelper.swift
+//  FindTheSide
+//
+//  Created by Yujia Gao on 10/20/18.
+//  Copyright © 2018 nosnaCo. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import UIKit
+
+class CoreDataHelper{
+    
+    static func saveLevel(){
+        
+    }
+    
+    static func retrieveLevel() -> Int{
+        let fetchRequest = NSFetchRequest<Int>(entityName: "Level")
+        d
+        return 0;
+    }
+}
